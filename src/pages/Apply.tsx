65d60c9ee5becd6e36daf1cc423984cf9994c228
JungAh Choi (최정아) <wjddk718@gmail.com>
2025-01-09 23:57:41 +0900

    카드 신청 예외 처리

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,15 +7,45 @@ import { updateApplyCard } from '@remote/apply'
 import { APPLY_STATUS } from '@models/apply'
 import useUser from '@hooks/auth/useUser'
 import { useParams, useNavigate } from 'react-router-dom'
-import { replace } from 'lodash'
+import useAppliedCard from '@components/apply/hooks/useAppliedCard'
+import { useAlertContext } from '@/contexts/AlertContext'
+import FullPageLoader from '@/components/shared/FullPageLoader'
 
 function ApplyPage() {
   const navigate = useNavigate()
+  const { open } = useAlertContext()
 
   const [readyToPoll, setReadyToPoll] = useState(false)
 
   const user = useUser()
   const { id } = useParams() as { id: string }
+
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied == null) {
+          return
+        }
+
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다.',
+            onButtonClick: () => {
+              window.history.back()
+            },
+          })
+
+          return
+        }
+
+        setReadyToPoll(true)
+      },
+      onError: () => {},
+      suspense: true,
+    },
+  })
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -52,8 +82,12 @@ function ApplyPage() {
     },
   })
 
+  if (data != null && data.status === APPLY_STATUS.COMPLETE) {
+    return null
+  }
+
   if (readyToPoll || 카드를신청중인가) {
-    return <div>Loading...</div>
+    return <FullPageLoader message="카드를 신청중입니다" />
   }
 
   return <Apply onSubmit={mutate} />
