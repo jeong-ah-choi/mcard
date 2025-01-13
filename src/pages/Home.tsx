ee3bdaf966ce9660cf230eb6b879a397ba221089
JungAh Choi (최정아) <wjddk718@gmail.com>
2025-01-13 18:47:59 +0900

    병목코드 제거 (정규식 활용), UI/UX 개선

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react'
 
 import AdBanners from '@/components/home/AdBanners'
 import CardList from '@/components/home/CardList'
-import Button from '@/components/shared/Button'
 import Top from '@/components/shared/Top'
 import ListRow from '@/components/shared/ListRow'
 
@@ -13,9 +12,7 @@ function HomePage() {
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택이 좋은 카드를 모아봤어요"
       />
-      <Button>안녕</Button>
       <AdBanners />
-      <Button>안녕</Button>
       <Suspense
         fallback={[...new Array(10)].map((_, idx) => (
           <ListRow.Skeleton key={idx} />
@@ -23,7 +20,6 @@ function HomePage() {
       >
         <CardList />
       </Suspense>
-      <Button>안녕</Button>
     </div>
   )
 }
